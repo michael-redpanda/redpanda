e41e0599e87ad5835e8d63ca75db1f871904a36b
Michael Boquard <michael@redpanda.com>
2023-06-26 08:25:42 -0400

    parameters

--- src/v/utils/debug_bundle.cc
+++ src/v/utils/debug_bundle.cc
@@ -11,11 +11,52 @@
 
 #include "debug_bundle.h"
 
+#include "utils/mutex.h"
 #include "vlog.h"
+
+#include <fmt/chrono.h>
 
 static ss::logger logger{"debug_bundle"};
 
 namespace debug_bundle {
+
+void debug_bundle::debug_bundle_parameters::operator()(std::vector<ss::sstring> & param_vector) const noexcept {
+
+    if (logs_since) {
+        param_vector.emplace_back("--logs-since");
+        param_vector.emplace_back(fmt::format(
+          "{:%Y-%m-%d %H:%M:%S}", fmt::localtime(logs_since.value())));
+    }
+
+    if (logs_until) {
+        param_vector.emplace_back("--logs-until");
+        param_vector.emplace_back(fmt::format(
+          "{:%Y-%m-%d %H:%M:%S}", fmt::localtime(logs_until.value())));
+    }
+
+    if (logs_size_limit) {
+        param_vector.emplace_back("--logs-size-limit");
+        param_vector.emplace_back(fmt::format("{}", logs_size_limit.value()));
+    }
+
+    if (metrics_interval) {
+        param_vector.emplace_back("--metrics-interval");
+        param_vector.emplace_back(fmt::format("{}s", metrics_interval.value().count()));
+    }
+}
+
+void debug_bundle::debug_bundle_credentials::operator()(std::vector<ss::sstring>& param_vector) const noexcept {
+    param_vector.emplace_back("--username");
+    param_vector.emplace_back(username);
+    param_vector.emplace_back("--password");
+    param_vector.emplace_back(password);
+    param_vector.emplace_back("--mechanism");
+    param_vector.emplace_back(mechanism);
+    if (use_tls) {
+        param_vector.emplace_back("--tls-enabled");
+    }
+}
+
 ss::future<debug_bundle::debug_bundle_stream_handler::consumption_result_type>
 debug_bundle::debug_bundle_stream_handler::operator()(
   debug_bundle::debug_bundle::debug_bundle_stream_handler::tmp_buf buf) {
@@ -32,5 +73,74 @@ debug_bundle::debug_bundle_stream_handler::operator()(
     return ss::make_ready_future<
       debug_bundle::debug_bundle_stream_handler::consumption_result_type>(
       ss::continue_consuming{});
+}
+
+ss::future<> debug_bundle::create_debug_bundle(
+  debug_bundle::debug_bundle::debug_bundle_parameters bundle_parameters,
+  std::optional<debug_bundle_credentials> credentials) {
+    // thread local mutex to prevent another shard from trying to fire off a new
+    // bundle process while one is in action
+    static thread_local mutex create_bundle_mutex;
+    gate_guard g{_gate};
+    auto try_result = create_bundle_mutex.try_get_units();
+    if (!try_result) {
+        throw std::system_error(
+          make_error_code(errc::debug_bundle_process_running));
+    }
+
+    if (ss::this_shard_id() != debug_bundle_shard_id) {
+        co_return co_await ss::do_with(
+          bundle_parameters,
+          credentials,
+          [this](auto& bundle_parameters, auto& credentials) {
+              return container().invoke_on(
+                debug_bundle_shard_id,
+                [bundle_parameters, credentials](debug_bundle& b) {
+                    return b.create_debug_bundle(
+                      bundle_parameters, credentials);
+                });
+          });
+    }
+
+    if (co_await is_running()) {
+        throw std::system_error(
+          make_error_code(errc::debug_bundle_process_running));
+    }
+
+    auto file_name = generate_file_name();
+    auto temporary_path = std::filesystem::path("/tmp")
+                          / std::filesystem::path(file_name);
+    auto final_path = _output_directory / std::filesystem::path(file_name);
+
+    auto params = generate_rpk_parameters(temporary_path, bundle_parameters, credentials);
+
+    std::vector<ss::sstring> env;
+
+
+    _rpk_process.emplace(::external_process<debug_bundle::debug_bundle_stream_handler>::create_external_process(params));
+}
+
+ss::sstring debug_bundle::generate_file_name() noexcept {
+    auto now = std::chrono::system_clock::now();
+    return fmt::format("{:%Y%m%d-%H%M%S}", now);
+}
+
+std::vector<ss::sstring> debug_bundle::generate_rpk_parameters(
+  const std::filesystem::path & output_path,
+  const debug_bundle_parameters& bundle_parameters,
+  const std::optional<debug_bundle_credentials>& credentials) const noexcept {
+    std::vector<ss::sstring> args;
+    args.emplace_back(_rpk_binary_path.string());
+    args.emplace_back("debug");
+    args.emplace_back("bundle");
+    args.emplace_back("--output");
+    args.emplace_back(output_path.string());
+    bundle_parameters(args);
+
+    if (credentials) {
+        credentials.value()(args);
+    }
+
+    return args;
 }
 } // namespace debug_bundle
--- src/v/utils/debug_bundle.h
+++ src/v/utils/debug_bundle.h
@@ -12,25 +12,78 @@
 #include "seastarx.h"
 #include "utils/external_process.h"
 #include "utils/gate_guard.h"
+#include "utils/human.h"
 
 #include <seastar/core/sharded.hh>
 
 namespace debug_bundle {
+
+enum class errc : int {
+    success = 0,
+    debug_bundle_process_running,
+};
+
+struct errc_category final : public std::error_category {
+    const char* name() const noexcept final { return "debug_bundle::errc"; }
+
+    std::string message(int c) const final {
+        switch (static_cast<errc>(c)) {
+        case errc::success:
+            return "success";
+        case errc::debug_bundle_process_running:
+            return "debug_bundle_process_running";
+        default:
+            return "debug_bundle::errc::unknown";
+        }
+    }
+};
+
+inline const std::error_category& error_category() noexcept {
+    static errc_category e;
+    return e;
+}
+
+inline std::error_code make_error_code(errc e) noexcept {
+    return std::error_code(static_cast<int>(e), error_category());
+}
 
 static constexpr ss::shard_id debug_bundle_shard_id = 0;
 
 class debug_bundle final : public ss::peering_sharded_service<debug_bundle> {
 public:
     struct debug_bundle_parameters {
-        std::optional<struct tm> logs_since;
-        std::optional<struct tm> logs_until;
-        std::optional<ss::sstring> logs_size_limit;
+        std::optional<std::chrono::time_point<std::chrono::system_clock>>
+          logs_since;
+        std::optional<std::chrono::time_point<std::chrono::system_clock>>
+          logs_until;
+        std::optional<human::golang_bytes> logs_size_limit;
+        std::optional<std::chrono::seconds> metrics_interval;
+
+        void operator()(std::vector<ss::sstring> & param_vector) const noexcept;
+    };
+    struct debug_bundle_credentials {
+        ss::sstring username;
+        ss::sstring password;
+        ss::sstring mechanism;
+        bool use_tls;
+
+        void operator()(std::vector<ss::sstring> & param_vector) const noexcept;
     };
     debug_bundle(
       std::filesystem::path output_directory,
       std::filesystem::path rpk_binary_path)
       : _output_directory(std::move(output_directory))
       , _rpk_binary_path(std::move(rpk_binary_path)) {}
+
+    ss::future<bool> is_running() noexcept {
+        co_return co_await container().invoke_on(
+          debug_bundle_shard_id,
+          [](debug_bundle& b) { return b._rpk_process.has_value(); });
+    }
+
+    ss::future<> create_debug_bundle(
+      debug_bundle_parameters bundle_parameters,
+      std::optional<debug_bundle_credentials> credentials);
 
     ss::future<> start();
 
@@ -43,13 +96,13 @@ private:
         using tmp_buf = stop_consuming_type::tmp_buf;
 
     public:
-        debug_bundle_stream_handler(
+        explicit debug_bundle_stream_handler(
           bool isstdout,
           std::optional<std::reference_wrapper<std::vector<ss::sstring>>>
             string_buffer
           = std::nullopt)
           : _isstdout(isstdout)
-          , _string_buffer(std::move(string_buffer)) {}
+          , _string_buffer(string_buffer) {}
 
         ss::future<consumption_result_type> operator()(tmp_buf buf);
 
@@ -59,9 +112,24 @@ private:
           _string_buffer;
     };
 
+    static ss::sstring generate_file_name() noexcept;
+    std::vector<ss::sstring> generate_rpk_parameters(
+      const std::filesystem::path & output_path,
+      const debug_bundle_parameters& bundle_parameters,
+      const std::optional<debug_bundle_credentials>& credentials)
+      const noexcept;
+
     std::filesystem::path _output_directory;
     std::filesystem::path _rpk_binary_path;
+    ss::sstring _home_dir;
+    ss::sstring _path_val;
     std::optional<external_process<debug_bundle_stream_handler>> _rpk_process;
+    ss::gate _gate;
 };
 
 } // namespace debug_bundle
+
+namespace std {
+template<>
+struct is_error_code_enum<debug_bundle::errc> : true_type {};
+} // namespace std
