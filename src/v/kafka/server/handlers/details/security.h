29984cf1ecf689a49615b4d9ab9d673ba45b2a1a
Michael Boquard <michael@redpanda.com>
2024-11-11 20:13:20 -0500

    kafka: Added security utilities for SCRAM messages
    Signed-off-by: Michael Boquard <michael@redpanda.com>

--- src/v/kafka/server/handlers/details/security.h
+++ src/v/kafka/server/handlers/details/security.h
@@ -14,6 +14,7 @@
 #include "kafka/server/request_context.h"
 #include "model/validation.h"
 #include "security/acl.h"
+#include "security/scram_credential.h"
 namespace kafka::details {
 
 /*
@@ -517,5 +518,16 @@ to_acl_binding_filter(const delete_acls_filter& filter) {
     return security::acl_binding_filter(
       to_resource_pattern_filter(filter), to_acl_entry_filter(filter));
 }
+
+template<typename T>
+concept KafkaUserName = requires(T item) {
+    { std::convertible_to<decltype(item.name()), security::credential_user> };
+};
+
+scram_mechanism
+security_to_kafka_mechanism(security::scram_algorithm_t mechanism);
+
+std::optional<security::scram_algorithm_t>
+kafka_to_security_mechanism(kafka::scram_mechanism mechanism);
 
 } // namespace kafka::details
