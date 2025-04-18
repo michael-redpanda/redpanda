94dd6575631ced4e946c878c9c2a2f2f1a749599
Michael Boquard <michael@redpanda.com>
2025-04-18 10:21:01 -0400

    model: Added panda_link to model
    Initial metadata definition
    
    Signed-off-by: Michael Boquard <michael@redpanda.com>

---
+++ src/v/model/panda_link.cc
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2025 Redpanda Data, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+#include "model/panda_link.h"
+
+#include <fmt/ranges.h>
+
+auto fmt::formatter<model::panda_link_metadata>::format(
+  const model::panda_link_metadata& m,
+  format_context& ctx) -> decltype(ctx.out()) {
+    auto out = ctx.out();
+    fmt::format_to(
+      out,
+      "{{name: \"{}\", uuid: {}, source_cluster_bootstrap_server: "
+      "{}}}",
+      m.name,
+      m.uuid,
+      m.source_cluster_bootstrap_server);
+    return out;
+}
+
+namespace model {
+
+std::ostream& operator<<(std::ostream& os, const panda_link_metadata& m) {
+    fmt::print(os, "{}", m);
+    return os;
+}
+} // namespace model
---
+++ src/v/model/panda_link.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2025 Redpanda Data, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+#pragma once
+
+#include "base/seastarx.h"
+#include "container/fragmented_vector.h"
+#include "model/metadata.h"
+#include "serde/envelope.h"
+#include "utils/named_type.h"
+#include "utils/uuid.h"
+
+#include <seastar/core/sstring.hh>
+
+#include <fmt/core.h>
+
+namespace model {
+
+/// @brief Unique identifier for a panda link
+using panda_link_id = named_type<uuid_t, struct panda_link_id_tag>;
+
+/**
+ * @brief Name of the link, which is provided by the user
+ */
+using panda_link_name = named_type<ss::sstring, struct panda_link_name_tag>;
+using panda_link_name_view
+  = named_type<std::string_view, struct panda_link_name_view_tag>;
+struct panda_link_metadata
+  : serde::envelope<
+      panda_link_metadata,
+      serde::version<0>,
+      serde::compat_version<0>> {
+    /// Name of the link
+    panda_link_name name;
+    /// UUID of each link
+    panda_link_id uuid;
+    /// Bootstrap server of the source cluster
+    ss::sstring source_cluster_bootstrap_server;
+
+    friend bool
+    operator==(const panda_link_metadata&, const panda_link_metadata&)
+      = default;
+
+    friend std::ostream& operator<<(std::ostream&, const panda_link_metadata&);
+
+    auto serde_fields() {
+        return std::tie(name, uuid, source_cluster_bootstrap_server);
+    }
+};
+} // namespace model
+
+template<>
+struct fmt::formatter<model::panda_link_metadata>
+  : fmt::formatter<string_view> {
+    auto format(const model::panda_link_metadata& m, format_context& ctx)
+      -> decltype(ctx.out());
+};
