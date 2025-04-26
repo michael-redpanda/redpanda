9cc29b63ff4f0826530125ef933a83fa64446ece
Michael Boquard <michael@redpanda.com>
2025-04-25 20:11:48 -0400

    panda_link: Added logger
    Signed-off-by: Michael Boquard <michael@redpanda.com>

---
+++ src/v/panda_link/logger.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2025 Redpanda Data, Inc.
+ *
+ * Licensed as a Redpanda Enterprise file under the Redpanda Community
+ * License (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * https://github.com/redpanda-data/redpanda/blob/master/licenses/rcl.md
+ */
+
+#pragma once
+
+#include "base/seastarx.h"
+
+#include <seastar/util/log.hh>
+
+namespace panda_link {
+extern ss::logger pllog;
+}
