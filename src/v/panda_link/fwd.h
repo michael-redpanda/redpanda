68f27c91addb583565fd4ebd9062b52b0c45a4ea
Michael Boquard <michael@redpanda.com>
2025-04-25 20:50:05 -0400

    panda_link: Add fwd
    Signed-off-by: Michael Boquard <michael@redpanda.com>

---
+++ src/v/panda_link/fwd.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2025 Redpanda Data, Inc.
+ *
+ * Licensed as a Redpanda Enterprise file under the Redpanda Community
+ * License (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * https://github.com/redpanda-data/redpanda/blob/master/licenses/rcl.md
+ */
+
+namespace panda_link {
+class write_at_offset_stm;
+class write_at_offset_stm_factory;
+} // namespace panda_link
