b78fbcda128f55cf70c49b0e68b15a794e63e46b
Michael Boquard <michael@redpanda.com>
2025-06-20 11:26:01 -0400

    c: Added commands to add update mirror topics
    Signed-off-by: Michael Boquard <michael@redpanda.com>

--- src/v/cluster/types.h
+++ src/v/cluster/types.h
@@ -3591,6 +3591,76 @@ struct remove_cluster_link_response
     auto serde_fields() { return std::tie(ec); }
 };
 
+struct add_mirror_topic_request
+  : serde::envelope<
+      add_mirror_topic_request,
+      serde::version<0>,
+      serde::compat_version<0>> {
+    using rpc_adl_exempt = std::true_type;
+
+    ::cluster_link::model::id_t link_id;
+    ::cluster_link::model::add_mirror_topic_cmd cmd;
+    model::timeout_clock::duration timeout{};
+
+    friend bool
+    operator==(const add_mirror_topic_request&, const add_mirror_topic_request&)
+      = default;
+
+    auto serde_fields() { return std::tie(link_id, cmd, timeout); }
+};
+
+struct add_mirror_topic_response
+  : serde::envelope<
+      add_mirror_topic_response,
+      serde::version<0>,
+      serde::compat_version<0>> {
+    using rpc_adl_exempt = std::true_type;
+
+    cluster_link::errc ec{cluster_link::errc::success};
+
+    friend bool operator==(
+      const add_mirror_topic_response&, const add_mirror_topic_response&)
+      = default;
+
+    auto serde_fields() { return std::tie(ec); }
+};
+
+struct update_mirror_topic_state_request
+  : serde::envelope<
+      update_mirror_topic_state_request,
+      serde::version<0>,
+      serde::compat_version<0>> {
+    using rpc_adl_exempt = std::true_type;
+
+    ::cluster_link::model::id_t link_id;
+    ::cluster_link::model::update_mirror_topic_state_cmd cmd;
+    model::timeout_clock::duration timeout{};
+
+    friend bool operator==(
+      const update_mirror_topic_state_request&,
+      const update_mirror_topic_state_request&)
+      = default;
+
+    auto serde_fields() { return std::tie(link_id, cmd, timeout); }
+};
+
+struct update_mirror_topic_state_response
+  : serde::envelope<
+      update_mirror_topic_state_response,
+      serde::version<0>,
+      serde::compat_version<0>> {
+    using rpc_adl_exempt = std::true_type;
+
+    cluster_link::errc ec{cluster_link::errc::success};
+
+    friend bool operator==(
+      const update_mirror_topic_state_response&,
+      const update_mirror_topic_state_response&)
+      = default;
+
+    auto serde_fields() { return std::tie(ec); }
+};
+
 } // namespace cluster
 
 namespace reflection {
