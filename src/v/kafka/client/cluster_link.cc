72dc3e43760399fff2c0cd00a5f4f691b3224ff6
Michael Boquard <michael@redpanda.com>
2025-07-15 12:14:40 -0400

    tmp

--- src/v/kafka/client/cluster_link.cc
+++ src/v/kafka/client/cluster_link.cc
@@ -11,6 +11,9 @@
 
 #include "kafka/client/cluster_link.h"
 
+#include "cluster_link/model/filter_utils.h"
+#include "cluster_link/model/types.h"
+#include "kafka/data/rpc/deps.h"
 #include "utils/prefix_logger.h"
 
 namespace kafka::client {
@@ -26,8 +29,20 @@ ss::future<> catch_and_log(const prefix_logger& logger, Func&& f) noexcept {
 }
 } // namespace
 
-cluster_link::cluster_link(const client_configuration& config)
-  : _logger(kclog, config.connection_cfg.client_id.value_or("kafka-monitoring"))
+using ::cluster_link::model::add_mirror_topic_cmd;
+using ::cluster_link::model::id_t;
+using data::rpc::topic_metadata_cache;
+
+static constexpr int32_t required_permissions
+  = 0x508; // DESCRIBE_CONFIG, DESCRIBE, READ
+
+cluster_link::cluster_link(
+  const client_configuration& config,
+  std::unique_ptr<link_registry> link_registry,
+  std::unique_ptr<topic_metadata_cache> topic_metadata_cache)
+  : _link_registry(std::move(link_registry))
+  , _topic_metadata_cache(std::move(topic_metadata_cache))
+  , _logger(kclog, config.connection_cfg.client_id.value_or("kafka-monitoring"))
   , _cluster(config.connection_cfg) {}
 
 ss::future<> cluster_link::connect() {
@@ -35,7 +50,133 @@ ss::future<> cluster_link::connect() {
     if (!_is_started) {
         co_await _cluster.start();
         _is_started = true;
+        _supported_api_versions_describe_configs
+          = co_await _cluster.supported_api_versions(describe_configs_api::key);
+        if (!_supported_api_versions_describe_configs) {
+            throw std::runtime_error(
+              "DescribeConfigs API is not supported by the cluster");
+        }
         vlog(_logger.trace, "Successfully connected to cluster");
+    }
+}
+
+ss::future<> cluster_link::update_mirror_topic_state(id_t link_id) {
+    vlog(_logger.trace, "Running update mirror topic state action");
+    vassert(
+      _is_started,
+      "Cluster link must be started before updating mirror topics");
+    vassert(
+      _supported_api_versions_describe_configs.has_value(),
+      "DescribeConfigs API is not supported by the cluster");
+
+    auto link_metadata = _link_registry->find_link_by_id(link_id);
+    if (!link_metadata) {
+        vlog(_logger.warn, "Link with id={} not found", link_id);
+        co_return;
+    }
+
+    const auto& auto_topic_create_settings
+      = link_metadata->state.auto_mirror_topic_task_config;
+
+    co_await _cluster.request_metadata_update();
+    auto& client_topic_cache = _cluster.get_topics();
+
+    for (const auto& [topic, md] : client_topic_cache.topics()) {
+        vlog(_logger.trace, "Checking topic {}", topic);
+        auto topic_config = _topic_metadata_cache->find_topic_cfg(
+          {model::kafka_namespace, topic});
+        if (topic_config.has_value()) {
+            vlog(_logger.trace, "Topic {} already exists, skipping", topic);
+            continue;
+        }
+
+        if (::cluster_link::model::select_topic(
+              topic, auto_topic_create_settings.filters)) {
+            vlog(_logger.trace, "Topic {} matched filter", topic);
+        } else {
+            vlog(_logger.trace, "Topic {} did not match filter", topic);
+            continue;
+        }
+        vlog(
+          _logger.trace,
+          "Checking permissions for topic {} ({:08x})",
+          topic,
+          md.authorized_operations);
+        if (
+          (md.authorized_operations & required_permissions)
+          != required_permissions) {
+            vlog(
+              _logger.trace,
+              "Not enough permissions to mirror topic {} ({:08x}). "
+              "Requires "
+              "{:08x}",
+              topic,
+              md.authorized_operations,
+              required_permissions);
+            continue;
+        }
+
+        vlog(_logger.info, "Topic {} is eligible for mirroring", topic);
+
+        describe_configs_response resp;
+        try {
+            resp = co_await describe_topic(topic);
+        } catch (const std::exception& e) {
+            vlog(
+              _logger.warn, "Failed to describe topic {}: {}", topic, e.what());
+            continue;
+        }
+        if (resp.data.results.empty()) {
+            vlog(
+              _logger.warn,
+              "Failed to describe topic {}: no resources found",
+              topic);
+            continue;
+        }
+        if (resp.data.results[0].error_code != error_code::none) {
+            vlog(
+              _logger.warn,
+              "Failed to describe topic {}: {} ({})",
+              topic,
+              resp.data.results[0].error_code,
+              resp.data.results[0].error_message);
+            continue;
+        }
+        if (resp.data.results[0].resource_name != topic) {
+            vlog(
+              _logger.warn,
+              "DescribeConfigs returned unexpected topic name: {} (expected "
+              "{})",
+              resp.data.results[0].resource_name,
+              topic);
+            continue;
+        }
+
+        auto& topic_configs = resp.data.results[0].configs;
+        chunked_hash_map<ss::sstring, ss::sstring> configs;
+        configs.reserve(topic_configs.size());
+        for (const auto& config : topic_configs) {
+            if (config.value.has_value()) {
+                configs.emplace(config.name, config.value.value());
+            }
+        }
+        vlog(_logger.trace, "Adding topic {} with configs: {}", topic, configs);
+
+        add_mirror_topic_cmd cmd{
+          .topic = topic,
+          .metadata = {
+            .source_topic_name = topic,
+            .destination_topic_id = ::model::topic_id(uuid_t::create()),
+            .topic_configs = std::move(configs)}};
+
+        auto ec = co_await _link_registry->add_mirror_topic(
+          link_id, std::move(cmd), model::timeout_clock::now());
+        if (ec != ::cluster::cluster_link::errc::success) {
+            vlog(_logger.warn, "Failed to add mirror topic {}: {}", topic, ec);
+            continue;
+        } else {
+            vlog(_logger.info, "Successfully added mirror topic {}", topic);
+        }
     }
 }
 
@@ -47,4 +188,24 @@ ss::future<> cluster_link::stop() noexcept {
 }
 
 prefix_logger& cluster_link::logger() { return _logger; }
+
+ss::future<describe_configs_response> cluster_link::describe_topic(
+  model::topic topic,
+  std::optional<chunked_vector<ss::sstring>> configuration_keys) {
+    vlog(_logger.trace, "Describing topic {}", topic);
+    describe_configs_request request;
+    request.data.resources.emplace_back(describe_configs_resource{
+      .resource_type = config_resource_type::topic,
+      .resource_name = std::move(topic),
+      .configuration_keys = std::move(configuration_keys)});
+
+    request.data.include_synonyms = false;
+    request.data.include_documentation = false;
+
+    co_return co_await _cluster.dispatch_to_any(
+      std::move(request),
+      std::min(
+        _supported_api_versions_describe_configs->max,
+        describe_configs_api::max_valid));
+}
 } // namespace kafka::client
--- src/v/kafka/client/cluster_link.h
+++ src/v/kafka/client/cluster_link.h
@@ -11,26 +11,69 @@
 
 #pragma once
 
+#include "cluster/cluster_link/errc.h"
+#include "cluster_link/model/types.h"
 #include "kafka/client/cluster.h"
 #include "kafka/client/configuration.h"
+#include "kafka/data/rpc/deps.h"
 #include "utils/prefix_logger.h"
 
 namespace kafka::client {
+
+class link_registry {
+public:
+    link_registry() = default;
+    link_registry(const link_registry&) = delete;
+    link_registry(link_registry&&) = delete;
+    link_registry& operator=(const link_registry&) = delete;
+    link_registry& operator=(link_registry&&) = delete;
+    virtual ~link_registry() = default;
+
+    virtual std::optional<::cluster_link::model::metadata>
+      find_link_by_id(::cluster_link::model::id_t) const = 0;
+
+    virtual ss::future<::cluster::cluster_link::errc> add_mirror_topic(
+      ::cluster_link::model::id_t,
+      ::cluster_link::model::add_mirror_topic_cmd,
+      model::timeout_clock::time_point)
+      = 0;
+};
+
 class cluster_link {
 public:
-    explicit cluster_link(const client_configuration& config);
+    cluster_link(
+      const client_configuration& config,
+      std::unique_ptr<link_registry> link_registry,
+      std::unique_ptr<data::rpc::topic_metadata_cache> topic_metadata_cache);
 
     /// Connects to all brokers
     ss::future<> connect();
     /// Disconnects from all brokers
     ss::future<> stop() noexcept;
 
+    /// This will:
+    /// - grab the list of topics from the remote cluster
+    /// - use the filters in the cluster link table to filter out the topics to
+    /// mirror
+    /// - for every topic that isn't yet mirrored, add that topic to the mirror
+    /// topic table
+    ss::future<> update_mirror_topic_state(::cluster_link::model::id_t link_id);
+
 private:
     prefix_logger& logger();
 
+    ss::future<describe_configs_response> describe_topic(
+      model::topic,
+      std::optional<chunked_vector<ss::sstring>> configuration_keys
+      = std::nullopt);
+
 private:
+    std::unique_ptr<link_registry> _link_registry;
+    std::unique_ptr<data::rpc::topic_metadata_cache> _topic_metadata_cache;
     prefix_logger _logger;
     cluster _cluster;
+
+    std::optional<api_version_range> _supported_api_versions_describe_configs;
 
     bool _is_started{false};
     ss::abort_source _as;
