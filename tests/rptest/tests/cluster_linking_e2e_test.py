e82bae20a4d555f2c9d4c560e765109e477a5630
Michael Boquard <michael@redpanda.com>
2025-08-22 16:12:16 -0400

    dt: Added initial tests of Shadow Linking API
    Signed-off-by: Michael Boquard <michael@redpanda.com>

--- tests/rptest/tests/cluster_linking_e2e_test.py
+++ tests/rptest/tests/cluster_linking_e2e_test.py
@@ -8,10 +8,16 @@
 # by the Apache License, Version 2.0
 
 from ducktape.mark import matrix
-from rptest.services.multi_cluster_services import Cluster, MultiClusterServices, ServiceType
-from rptest.tests.redpanda_test import RedpandaTest
 
+from connectrpc.errors import ConnectError, ConnectErrorCode
+from google.protobuf import duration_pb2
+
+from rptest.clients.admin.v2 import Admin as AdminV2
+from rptest.clients.admin.proto.redpanda.core.admin import admin_pb2, admin_pb2_connect, shadow_link_pb2, shadow_link_pb2_connect
 from rptest.services.cluster import cluster
+from rptest.services.multi_cluster_services import Cluster, MultiClusterServices, ServiceType
+from rptest.services.redpanda import LoggingConfig
+from rptest.tests.redpanda_test import RedpandaTest
 from rptest.util import expect_exception, wait_until_result
 
 
@@ -117,3 +123,142 @@ class MultiClusterKafkaTest(MultiClusterTestBase):
             assert services.secondary.is_kafka, \
                 f"Expected Kafka service, got {services.secondary}"
             self.basic_ops(services)
+
+
+class ShadowLinkBasicTests(RedpandaTest):
+    def __init__(self, test_context, *args, **kwargs):
+        super().__init__(test_context=test_context,
+                         num_brokers=3,
+                         log_config=LoggingConfig(
+                             'info', logger_levels={'cluster_link': 'trace'}),
+                         *args,
+                         **kwargs)
+
+        self.test_context = test_context
+        self.admin_v2: AdminV2 = None
+        self.services: MultiClusterServices = None
+
+    def setUp(self):
+        self.services = MultiClusterServices(
+            self.test_context,
+            self.logger,
+            self.redpanda,
+            secondary_type=ServiceType.REDPANDA,
+            num_brokers=3)
+        self.services.setUp()
+        self.services.primary.service.enable_development_feature_support()
+        self.services.primary.service.set_cluster_config(
+            values={"development_enable_cluster_link": True})
+        self.admin_v2 = AdminV2(self.services.primary.service)
+
+    def list_all_shadow_links(
+        self, client: shadow_link_pb2_connect.ShadowLinkServiceClient
+    ) -> list[shadow_link_pb2.ShadowLink]:
+        resp = client.call_list_shadow_links(
+            req=shadow_link_pb2.ListShadowLinksRequest())
+        if resp.error() is not None:
+            raise resp.error()
+        return [link for link in resp.message().shadow_links]
+
+    def get_shadow_link(
+            self, client: shadow_link_pb2_connect.ShadowLinkServiceClient,
+            name: str) -> shadow_link_pb2.ShadowLink:
+        resp = client.call_get_shadow_link(
+            req=shadow_link_pb2.GetShadowLinkRequest(name=name))
+        if resp.error() is not None:
+            raise resp.error()
+        return resp.message().shadow_link
+
+    @cluster(num_nodes=6)
+    def test_service_exists(self):
+        admin: admin_pb2_connect.AdminServiceClient = self.admin_v2.admin()
+        rpc_routes: admin_pb2.ListRPCRoutesResponse = admin.call_list_rpc_routes(
+            req=admin_pb2.ListRPCRoutesRequest()).message()
+        self.logger.info(f'Routes: {rpc_routes.routes}')
+
+        expected_routes: list[admin_pb2.RPCRoute] = [
+            admin_pb2.RPCRoute(
+                name="redpanda.core.admin.ShadowLinkService.CreateShadowLink",
+                http_route=
+                "/v2/redpanda.core.admin.ShadowLinkService/CreateShadowLink"),
+            admin_pb2.RPCRoute(
+                name="redpanda.core.admin.ShadowLinkService.DeleteShadowLink",
+                http_route=
+                "/v2/redpanda.core.admin.ShadowLinkService/DeleteShadowLink"),
+            admin_pb2.RPCRoute(
+                name="redpanda.core.admin.ShadowLinkService.GetShadowLink",
+                http_route=
+                "/v2/redpanda.core.admin.ShadowLinkService/GetShadowLink"),
+            admin_pb2.RPCRoute(
+                name="redpanda.core.admin.ShadowLinkService.ListShadowLinks",
+                http_route=
+                "/v2/redpanda.core.admin.ShadowLinkService/ListShadowLinks"),
+            admin_pb2.RPCRoute(
+                name="redpanda.core.admin.ShadowLinkService.UpdateShadowLink",
+                http_route=
+                "/v2/redpanda.core.admin.ShadowLinkService/UpdateShadowLink"),
+            admin_pb2.RPCRoute(
+                name="redpanda.core.admin.ShadowLinkService.FailOver",
+                http_route="/v2/redpanda.core.admin.ShadowLinkService/FailOver"
+            ),
+        ]
+
+        # Check that all expected routes are present in the actual routes
+        actual_routes = list(rpc_routes.routes)
+        for expected in expected_routes:
+            assert expected in actual_routes, f"Expected route {expected} not found in {actual_routes}"
+
+    @cluster(num_nodes=6)
+    def test_create_simple_link(self):
+        shadow_link: shadow_link_pb2_connect.ShadowLinkServiceClient = self.admin_v2.shadow_link(
+        )
+        create_shadow_link_request = shadow_link_pb2.CreateShadowLinkRequest()
+        shadow_link_resource = shadow_link_pb2.ShadowLink()
+        self.services.secondary.service.nodes
+        shadow_link_resource.name = "test-link"
+        client_options = shadow_link_pb2.ShadowLinkClientOptions(
+            bootstrap_servers=[
+                f"{node.name}:9092"
+                for node in self.services.secondary.service.nodes
+            ])
+        self.logger.debug(
+            f'Bootstrap servers: {client_options.bootstrap_servers}')
+
+        shadow_link_configurations = shadow_link_pb2.ShadowLinkConfigurations(
+            client_options=client_options)
+        shadow_link_resource.configurations.CopyFrom(
+            shadow_link_configurations)
+        create_shadow_link_request.shadow_link.CopyFrom(shadow_link_resource)
+
+        shadow_link_create_result = shadow_link.call_create_shadow_link(
+            req=create_shadow_link_request)
+        if shadow_link_create_result.error() is not None:
+            assert False, f"Failed to create link: {shadow_link_create_result.error()}"
+        self.logger.info(
+            f'Create shadow link result: {shadow_link_create_result.message().shadow_link}'
+        )
+
+        created_shadow_link: shadow_link_pb2.ShadowLink = shadow_link_create_result.message(
+        ).shadow_link
+
+        assert created_shadow_link.name == "test-link", \
+            f"Expected shadow link name to be 'test-link', but got {created_shadow_link.name}"
+
+        assert created_shadow_link.configurations.topic_metadata_sync_options.interval == duration_pb2.Duration(seconds=0), \
+            f"Expected interval to be '0' but got {created_shadow_link.configurations.topic_metadata_sync_options.interval}"
+
+        shadow_links = self.list_all_shadow_links(client=shadow_link)
+        assert len(
+            shadow_links
+        ) == 1, f"Expected exactly one shadow link, got {len(shadow_links)}"
+        assert shadow_links[
+            0].name == "test-link", f"Expected shadow link name to be 'test-link', got {shadow_links[0].name}"
+
+        got_link = self.get_shadow_link(client=shadow_link, name="test-link")
+        assert got_link.name == "test-link", f"Expected shadow link name to be 'test-link', got {got_link.name}"
+
+        try:
+            self.get_shadow_link(client=shadow_link, name="non-existent-link")
+            assert False, "Should not have gotten a non-existant link"
+        except ConnectError as e:
+            assert e.code == ConnectErrorCode.NOT_FOUND, f"Expected NOT_FOUND error code, got {e.code}"
